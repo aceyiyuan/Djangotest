38232ad83df19f560360ec62078c978c91aedb10
yiyuannew <yiyuan.new@gmail.com>
2022-08-26 17:05:19 +0300

    reset password

--- crm/settings.py
+++ crm/settings.py
@@ -131,3 +131,19 @@ MEDIA_ROOT= BASE_DIR/'static/images'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER="org.sugar@gmail.com"
+EMAIL_HOST_PASSWORD="hajxwjgjpehphlsj"
+
+
+
+
+
+
