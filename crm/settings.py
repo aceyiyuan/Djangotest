b89d50b78bf7cb977bd37610c6e06ddbe9dae288
yiyuannew <yiyuan.new@gmail.com>
2022-08-26 17:23:40 +0300

    settings issue

--- crm/settings.py
+++ crm/settings.py
@@ -139,8 +139,9 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="org.sugar@gmail.com"
-EMAIL_HOST_PASSWORD="hajxwjgjpehphlsj"
+EMAIL_HOST_USER="sugar@gmail.com"
+EMAIL_HOST_PASSWORD="hajxwjgjpehphlsj" #app password sample. invalid password:)
+
 
 
 
