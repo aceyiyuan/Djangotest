37ad7235f40246ea5edd78c61a844fbf63e5d94f
yiyuannew <yiyuan.new@gmail.com>
2022-09-02 12:33:51 +0300

    messgae

--- crm/settings.py
+++ crm/settings.py
@@ -120,7 +120,7 @@ MEDIA_URL= '/images/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT= BASE_DIR/'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
